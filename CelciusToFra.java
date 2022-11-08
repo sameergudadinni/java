b0d13ff5e9a20cc1b712c391b617b4785d33ecbb
Sameer Gudadinni <117703668+sameergudadinni@users.noreply.github.com>
2022-11-08 13:29:00 +0530

    Create CelciusToFra.java

---
+++ CelciusToFra.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public static void main(String[]args){
+  Scanner sc=new Scanner(System.in);
+  
+   int C=sc.nextInt();
+   int T=0;
+  T = (C * 9)/5 +32;
+  System.out.println(T);
+}
