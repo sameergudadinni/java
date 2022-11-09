1adaedd38d0f15d1c06aa79eb89616a6ae9c8418
Sameer Gudadinni <117703668+sameergudadinni@users.noreply.github.com>
2022-11-09 09:10:20 +0530

    Create ifElse.java

---
+++ ifElse.java
@@ -0,0 +1,25 @@
+public static void main(String[]args){
+    int age=20;
+   switch(age){
+   
+      case 10:
+       System.out.println("Age 10");
+       break;
+        case 15:
+       System.out.println("Age 15");
+       break;
+        case 20:
+       System.out.println("Age 20");
+       break;
+        case 25:
+       System.out.println("Age 25");
+       break;
+       defualt:
+       System.out.println("No Match");
+   }
+    System.out.println("Done with switching");
+}
+
+output
+20
+Done with switching
