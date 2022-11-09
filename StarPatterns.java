77ec65d0d1734e152da883aa99c9a0500dd5fc62
Sameer Gudadinni <117703668+sameergudadinni@users.noreply.github.com>
2022-11-09 12:00:18 +0530

    Create StarPatterns.java

---
+++ StarPatterns.java
@@ -0,0 +1,48 @@
+1)increasing order
+
+import java.util.Scanner;
+public static void main(String[]args){
+  Scanner sc=new Scanner(System.in);
+  
+  int n=sc.nextInt();
+  
+  for(int i=1; i<=n; i++){
+   for(int j=1; j<=i; j++){
+    System.out.print("");
+   }
+      System.out.println();
+  }
+}
+
+output
+// in increasing order
+#
+##
+###
+####
+
+2)Deacresing order
+
+import java.util.Scanner;
+class Main{
+public static void main(String[]args){
+   int n=sc.nextInt();
+  
+  for(int i=1; i<=n; i++){
+    for(int j=i; j<=n; j++){
+      System.out.print("#");
+    }
+    System.out.println();
+  }
+}
+}
+
+output
+####
+###
+##
+#
+
+
+   
+   
