7da75223012af2591410012d1965739eeccebf53
Sameer Gudadinni <117703668+sameergudadinni@users.noreply.github.com>
2022-11-09 09:16:48 +0530

    Create ifElse2.java

---
+++ ifElse2.java
@@ -0,0 +1,17 @@
+public static void main(String[]args){
+   int a=50;
+  if(a<10){
+    System.out.println("A");
+    
+   }
+  else if(a<=20){
+     System.out.println("B");
+  }
+  else if(a<20){
+     System.out.println("C");
+  }
+  else{
+     System.out.println("D");
+  }
+  
+}
