4984c5b3c89b57adeda39761e7fc93dd86bb9367
Sameer Gudadinni <117703668+sameergudadinni@users.noreply.github.com>
2022-11-09 12:40:37 +0530

    Update StarPatterns.java

--- StarPatterns.java
+++ StarPatterns.java
@@ -93,7 +93,60 @@ public class Main
 }
 
 output
-            #
+            ##
 	   ####
 	  ######
 	 ########
+	  5) Both sided Triengle tip
+	  import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	
+	for(int i=1; i<=n; i++){
+	    for(int j=i; j<=n; j++){
+	        System.out.print(" ");
+	    }
+	    for(int j=1; j<i; j++){
+	        System.out.print("#"); 
+	    }
+	    for(int j=1; j<=i; j++){
+	        System.out.print("#"); 
+	    }
+	    System.out.println();  
+	}
+	}
+}
+4) Left Triengle
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	Scanner sc=new Scanner(System.in);
+	 int n=sc.nextInt();
+	 for(int i=1; i<=n; i++){
+	     
+	 
+      for(int j=1; j<=i; j++){
+     System.out.print(" "); 
+      } 
+          for(int j=i; j<=n; j++){
+              
+              System.out.print("#");
+          }
+           System.out.println();
+      
+	}
+	}
+}
+	
+output
+    ######
+     #####
+      ####
+       ###
+        ##
+	 #
