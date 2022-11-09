8a8343f634b56e2212bc1c1dc1a1fb2e4bafe65a
Sameer Gudadinni <117703668+sameergudadinni@users.noreply.github.com>
2022-11-09 11:14:54 +0530

    Update rightTriengle.java

--- rightTriengle.java
+++ rightTriengle.java
@@ -22,3 +22,28 @@ public static void main(String[]args){
       System.out.println();
   }
 }
+
+2) 
+  public static void main(String[]args){
+      Scanner sc=new Scanner(System.in);
+  
+     int n=sc.nextInt();
+  
+      for(int i=1; i<=n; i++){
+       for(int j=1; j<=n; j++){
+         System.out.print(" ");
+       } 
+        for(int k=1; k<=i; k++)
+        {
+         System.out.print("#");
+          
+        } 
+        System.out.println("");
+      }
+}
+output
+       #
+      ##
+     ###
+    ####
+     
