3cda24c88e95506d543cf43a4339d02d8b9a9a94
Sameer Gudadinni <117703668+sameergudadinni@users.noreply.github.com>
2022-11-13 15:36:51 +0530

    Create ciepher.java

---
+++ ciepher.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+public class{
+
+   public static void main(String[]args){
+   
+   
+      Scanner sc=new Scanner(System.in);
+     
+     String a=sc.nextLine();
+     int b=a.length();
+     
+     for(int i=0; i<b; i++){
+       char result=a.charAt(i);
+     
+        Switch(result){
+        
+          case 'A':
+          System.out.println("0");
+          break;
+             case 'B':
+          System.out.print("1");
+          break;
+          case 'C':
+          System.out.print("2");
+          break;
+          case 'D':
+          System.out.print("3");
+          break;
+          case 'E':
+          System.out.print("4");
+          break;
+          case 'F':
+          System.out.print("5");
+          break;
+          case 'G':
+          System.out.print("6");
+          break;
+          case 'H':
+          System.out.print("7");
+          break;
+          case 'I':
+          System.out.print("8");
+          break;
+          case 'J':
+          System.out.print("9");
+          break;
+          case 'K':
+          System.out.print("10");
+          break;
+          case 'L':
+          System.out.print("11");
+          break;
+          case 'M':
+          System.out.print("12");
+          break;
+          case 'N':
+          System.out.print("13");
+          break;
+          case 'O':
+          System.out.print("14");
+          break;
+          case 'P':
+          System.out.print("15");
+          break;
+          case 'Q':
+          System.out.print("16");
+          break;
+          case 'R':
+          System.out.print("17");
+          break;
+          case 'S':
+          System.out.print("18");
+          break;
+          case 'T':
+          System.out.print("19");
+          break;
+          case 'U':
+          System.out.print("20");
+          break;
+          case 'V':
+          System.out.print("21");
+          break;
+          case 'W':
+          System.out.print("22");
+          break;
+          case 'X':
+          System.out.print("23");
+          break;
+          case 'Y':
+          System.out.print("24");
+          break;
+          case 'Z':
+          System.out.print("25");
+          break;
+         
+        }
+     }
+     
+   }
+}
